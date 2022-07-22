8a9b4fc4d9e00feeae89749c55e454af0bc77af2
Darian Chen <darian.chen8639@gmail.com>
2022-07-22 12:50:28 -0700

    added red text if counter goes negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
     function counter() {
       let total = 140 - $(this).val().length;
       $(".counter").text(total);
+      if(total < 0) { 
+        $(".counter").css("color", "red")
+      }
     }
+  
 
     $(textCount).keyup(counter);
     $(textCount).change(counter);
