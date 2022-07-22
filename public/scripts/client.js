8212fb8acbeaf8a23a917b5b7f9a34d4fcc8e84a
Darian Chen <darian.chen8639@gmail.com>
2022-07-22 12:50:58 -0700

    fixed dynamic tweets from being hardcoded

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,59 +7,68 @@
 // Fake data taken from initial-tweets.json
 const data = [
     {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+        "user": {
+            "name": "Newton",
+            "avatars": "https://i.imgur.com/73hZDYK.png"
+            ,
+            "handle": "@SirIsaac"
+        },
+        "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "created_at": 1461116232227
     },
     {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+        "user": {
+            "name": "Descartes",
+            "avatars": "https://i.imgur.com/nlhLi3I.png",
+            "handle": "@rd"
+        },
+        "content": {
+            "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
     }
-  ]
+]
 
-const renderTweets = function(tweets) {
+const renderTweets = function (tweets) {
     for (let tweet of tweets) {
         let $tweet = createTweetElement(tweet);
-        $('#tweets-container').prepend($tweet);
-      }
+        $('.tweets-container').prepend($tweet);
     }
+}
 
-const createTweetElement = function(tweet) {
-  let $tweet = (
-  `<article class="">
+const createTweetElement = function (tweet) {
+    let $tweet = (
+        `<article class="">
 
   <div class="article-header">
     <p>
-      <img class="avatar" src="https://i.imgur.com/73hZDYK.png">
-      <span>Random Name
+      <img class="avatar" src="${tweet.user.avatars}">
+      <span>${tweet.user.name}
         </span>
         </p>
-      <p class="username">@tweeter-user</p>
+      <p class="username">${tweet.user.handle}</p>
   </div>
 
-  <p class="container-tweet">tweet tweet tweet tweet...</p>
-  <footer class="time">A few days ago</footer>
+  <p class="container-tweet">${tweet.content.text}</p>
+  <footer class="time">${tweet.created_at}</footer>
   <div class="icons">
   <i class="fa-solid fa-flag"></i>
   <i class="fa-solid fa-retweet"></i>
   <i class="fa-solid fa-heart"></i>
 </div>
   </article>`);
-  
-  return $tweet;
+
+    return $tweet;
+}
+
+const loadtweets = () => {
+    $.ajax("/tweets/", { method: "GET" })
+        .then(function (data) {
+            renderTweets(data)
+        }
+        )
 }
 
 renderTweets(data);
