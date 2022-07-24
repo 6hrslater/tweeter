0082f4a8c43fb7ddc4c6c3d5eb51c201d64c8561
Darian Chen <darian.chen8639@gmail.com>
2022-07-23 18:37:24 -0700

    finalized error functions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     const textCount = ".new-tweet textarea";
     function counter() {
       let total = 140 - $(this).val().length;
-      $(".counter").text(total);
+      $(".counter").text(total).css("color", "black");
       if(total < 0) { 
         $(".counter").css("color", "red")
       }
